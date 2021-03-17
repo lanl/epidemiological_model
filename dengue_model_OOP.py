e810cc8000cb01ee87d75ce9490075946c6c9f15
Jeffrey Keithley <jkeithley@lanl.gov>
2021-03-17 13:17:24 -0600

    Reorganized graphing function

--- dengue_model_OOP.py
+++ dengue_model_OOP.py
@@ -60,23 +60,25 @@ class DengueSEIRModel(VectorBorneDiseaseModel):
     
     def graph_model(self):
         Sh, Eh, Iha, Ihs, Rh, Sv, Ev, Iv = self.model_output.T
+        
         fig = plt.figure(facecolor='w', figsize=[2*6.4, 2*4.8])
         ax = fig.add_subplot(111, facecolor='#dddddd', axisbelow=True)
-        ax.plot(self.t, Sh, 'b', alpha=0.5, lw=2, label='Susceptible Humans')
-        ax.plot(self.t, Iha+Ihs, 'r', alpha=0.5, lw=2, label='Infected Humans')
-        ax.plot(self.t, Iv, 'k', alpha=0.5, lw=2, label='Infected Vectors')
-        #ax.plot(t, R, 'g', alpha=0.5, lw=2, label='Recovered')
-
         ax.set_xlabel('Time (days)')
         ax.set_ylabel('Cases')
         ax.yaxis.set_tick_params(length=0)
         ax.xaxis.set_tick_params(length=0)
         ax.grid(b=True, which='major', c='w', lw=2, ls='-')
-        legend = ax.legend()
-        plt.title("Dengue Incidence")
-        legend.get_frame().set_alpha(0.5)
+        
         for spine in ('top', 'right', 'bottom', 'left'):
-            ax.spines[spine].set_visible(False)
+           ax.spines[spine].set_visible(False)
+        
+        ax.plot(self.t, Sh, 'b', alpha=0.5, lw=2, label='Susceptible Humans')
+        ax.plot(self.t, Iha+Ihs, 'r', alpha=0.5, lw=2, label='Infected Humans')
+        ax.plot(self.t, Iv, 'k', alpha=0.5, lw=2, label='Infected Vectors')
+        legend = ax.legend()
+        legend.get_frame().set_alpha(0.5)
+        plt.title("Dengue Incidence")
+
         plt.show()
         
 def main(): 
