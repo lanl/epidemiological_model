a97b7146e4d8be1f3498bba42f071b25bb8f92f3
Jeffrey Keithley <jkeithley@lanl.gov>
2021-04-29 11:33:53 -0600

    Made logfile archive folder

--- Epi_SEIR/clean.sh
+++ Epi_SEIR/clean.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-rm logs/*.log
-rm human_model_output/*.csv
-rm human_model_output/*.parquet
+LOGS_DIR="/Users/jkeithley/Documents/CIMMID/human/dengue_model/epi_seir/logs"
+HUMAN_OUTPUT_DIR="/Users/jkeithley/Documents/CIMMID/human/dengue_model/epi_seir/human_model_output"
+
+mv $LOGS_DIR/*.log $LOGS_DIR/logfile_archive
+rm $HUMAN_OUTPUT_DIR/*.csv
+rm $HUMAN_OUTPUT_DIR/*.parquet
