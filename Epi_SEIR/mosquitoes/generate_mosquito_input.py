0fe7011a3260242f4392b1881a34ff714367b96d
Jeffrey Keithley <jkeithley@lanl.gov>
2021-04-29 11:19:30 -0600

    Added script to make dummy mosquito population input based on the simulation duration

---
+++ Epi_SEIR/mosquitoes/generate_mosquito_input.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import random
+import numpy as np
+import yaml
+
+with open('/Users/jkeithley/Documents/CIMMID/human/dengue_model/Epi_SEIR/config/config.yaml', 'r') as in_file:
+    n = yaml.safe_load(in_file)['DURATION']
+
+arr = np.random.randint(1000, 1500, size=n)
+
+df = pd.DataFrame({'Sv': arr})
+
+df.to_csv('/Users/jkeithley/Documents/CIMMID/human/dengue_model/epi_seir/mosquitoes/mosq.csv')
