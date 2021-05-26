3984ea82998eb61cbeae62160e7d4749e335073a
Nidhi Parikh <nidhip@lanl.gov>
2021-05-26 11:27:31 -0600

    Minor updates to config and run_human_epi_model.sh files for integration purpose

--- Epi_SEIR/run_human_epi_model.sh
+++ Epi_SEIR/run_human_epi_model.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 
+# Takes two arguments:
+# 1. Config file (e.g., ./config/human_epi_config.yaml)
+# 2. A flag that is set to 1 if one needs to run clean.sh and 0 otherwise
+
 if [ "$1" == "" ]; then
-	echo "Error: Must specify configuration file path"
-	exit 1
+    echo "Error: Must specify configuration file path"
+    exit 1
 fi
 
 CONFIG_FILE_PATH=$1
+NEED_TO_CLEANUP=$2
 
-./clean.sh $CONFIG_FILE_PATH
+if [ "$NEED_TO_CLEANUP" -eq 1 ]; then
+    ./clean.sh $CONFIG_FILE_PATH
+fi
 
 # generate mosquito input for development of model
 python generate_inputs.py -c $CONFIG_FILE_PATH 
@@ -15,5 +22,3 @@ python generate_inputs.py -c $CONFIG_FILE_PATH
 python models_main.py -c $CONFIG_FILE_PATH -d dengue
 python models_main.py -c $CONFIG_FILE_PATH -d wnv
 
-echo " "
-cat logs/*.log
