cc3f2a5d51c9776837d292eea13466b56a724301
Jeffrey Keithley <jkeithley@lanl.gov>
2021-06-30 15:26:03 -0600

    Changed main() function structure

--- Epi_SEIR/models_main.py
+++ Epi_SEIR/models_main.py
@@ -10,10 +10,8 @@ given paramters from config.yaml configuration file.
 
 from dengue import DengueSEIRModel
 from wnv import WNVSEIRModel
-#import vbdm
 from utils import create_arg_parser
 import sys
-from logging import shutdown
 
 
 def main():
@@ -25,21 +23,17 @@ def main():
         parser = create_arg_parser()
         args, unknown = parser.parse_known_args()
 
-    # config_file = args.config_file
     disease_name = args.disease_name.lower()
 
     if disease_name == 'dengue':
-        den = DengueSEIRModel(args.config_file)
-        den.logger.info(den)
-        den.run_model()
-        den.save_output(disease_name)
-        den.logger.info('SUCCESS')
+        disease = DengueSEIRModel(args.config_file)
     elif disease_name == 'wnv':
-        wnv = WNVSEIRModel(args.config_file)
-        wnv.logger.info(wnv)
-        wnv.run_model()
-        wnv.save_output(disease_name)
-        wnv.logger.info('SUCCESS')
+        disease = WNVSEIRModel(args.config_file)
+
+    disease.logger.info(disease)
+    disease.run_model()
+    disease.save_output(disease_name)
+    disease.logger.info('SUCCESS')
 
 
 if __name__ == "__main__":
