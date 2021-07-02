81f3cc507dbeb1819940b8158808e6c076bb01a9
Jeffrey Keithley <jkeithley@lanl.gov>
2021-07-02 15:40:00 -0600

    Added ValueError testing for initial states

--- Epi_SEIR/test_monkey.py
+++ Epi_SEIR/test_monkey.py
@@ -3,13 +3,20 @@ from dengue import DengueSEIRModel
 from wnv import WNVSEIRModel
 import sys
 
-arg_list = ['config/pytest_config_1.yaml',
-            'config/pytest_config_2.yaml']
+arg_list = ['config/pytest_config_1.yaml']
+            #'config/pytest_config_2.yaml']
+
+"""
+TODO: have set of config files with things that should raise a type of error
+such as IO errors, ValueError, etc. each of these error types gets it own test function
+with the list of these config files that triggers them as an arglist.
+"""
 
 
 class TestDengue:
     @pytest.fixture
     def setup_dengue(self, config_file):
+        return 0
         disease = DengueSEIRModel(config_file)
 
         disease.logger.info(disease)
@@ -25,13 +32,16 @@ class TestDengue:
         with monkeypatch.context() as m:
             m.setattr(sys, 'argv', ['models_main', '-c', config_file, '-d', 'dengue'])
             # print('\033[7m' + "FLAG ----------" + '\033[0m', setup_dengue.states)
-
-            assert True
+            with pytest.raises(ValueError):
+                disease = DengueSEIRModel(config_file)
+            # models_main.main()
+            # assert True
 
 
 class TestWNV:
     @pytest.fixture
     def setup_wnv(self, config_file):
+        return 0
         disease = WNVSEIRModel(config_file)
 
         disease.logger.info(disease)
@@ -41,7 +51,7 @@ class TestWNV:
 
         return disease
 
-    # @pytest.mark.skip
+    @pytest.mark.skip
     @pytest.mark.parametrize("config_file", arg_list)
     def test_wnv(self, setup_wnv, monkeypatch, config_file):
         with monkeypatch.context() as m:
