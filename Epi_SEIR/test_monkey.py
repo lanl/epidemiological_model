f84bcb90a23f23a5f3d9c9f52a7a73eaef96b354
Jeffrey Keithley <jkeithley@lanl.gov>
2021-07-08 14:40:31 -0600

    Added tests for mosquito input

--- Epi_SEIR/test_monkey.py
+++ Epi_SEIR/test_monkey.py
@@ -11,10 +11,12 @@ value_error_arglist = ['config/unit_testing/positive_duration.yaml',
                        'config/unit_testing/duration_le_mosq.yaml',
                        'config/unit_testing/unique_position.yaml',
                        'config/unit_testing/positive_position.yaml',
-                       'config/unit_testing/positive_states.yaml']
+                       'config/unit_testing/positive_states.yaml',
+                       'config/unit_testing/mosq_positive.yaml']
 type_error_arglist = ['config/unit_testing/strings.yaml',
                       'config/unit_testing/position_integers.yaml',
-                      'config/unit_testing/numerical_states.yaml']
+                      'config/unit_testing/numerical_states.yaml',
+                      'config/unit_testing/mosq_numerical.yaml']
 
 """
 TODO: have set of config files with things that should raise a type of error
