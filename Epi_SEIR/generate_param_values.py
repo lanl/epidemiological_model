affd1128d49b8f7c054d929f7b945255a4804d59
mbarnard <mbarnard@lanl.gov>
2021-08-25 11:08:23 -0600

    Creates csv of parameter values to sequence through

---
+++ Epi_SEIR/generate_param_values.py
@@ -0,0 +1,30 @@
+import pandas as pd
+#import random
+import numpy as np
+import yaml
+#import pyarrow as pa
+#import pyarrow.parquet as pq
+import os
+import argparse
+
+
+def param_data(disease_name):
+    """Generates input data for sequencing through parameter values
+    """
+    with open('config/local_param_config.yaml', 'r') as in_file:
+        config_dict = yaml.safe_load(in_file)
+    
+    keys = list(config_dict[disease_name.upper()].keys())
+    out = []
+    mult = 1
+    for i in range(0,len(keys)):
+        start = config_dict[disease_name.upper()][keys[i]]['start']
+        stop = config_dict[disease_name.upper()][keys[i]]['stop']
+        step = config_dict[disease_name.upper()][keys[i]]['step']
+        out.append(np.arange(start, stop, step))
+        mult = mult * len(out[i])
+    
+    #get df of all possible combinations of the parameters       
+    df = pd.DataFrame(np.array(np.meshgrid(*out)).reshape(len(keys), mult).T, columns = keys) 
+    output_path = os.path.join(config_dict['OUTPUT_DIR'], f'{disease_name}_param_values.csv')
+    df.to_csv(output_path, index = False)
