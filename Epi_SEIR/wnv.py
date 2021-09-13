1fff76ca1c5fc756fe64f919d617df79d06c1a18
mbarnard <mbarnard@lanl.gov>
2021-09-13 16:01:42 -0600

    Add Rb compartment, switch to constant K_v,r_v

--- Epi_SEIR/wnv.py
+++ Epi_SEIR/wnv.py
@@ -47,46 +47,48 @@ class WNVSEIRModel(vbdm.VectorBorneDiseaseModel):
     def _population_sizes(self):
         """Calculates population sizes of human and vector compartments"""
         self.Nv = sum([self.states['Sv'], self.states['Ev'], self.states['Iv']])
-        self.Nb = sum([self.states['Sb'], self.states['Eb'], self.states['Ib']])
+        self.Nb = sum([self.states['Sb'], self.states['Eb'], self.states['Ib'], self.states['Rb']])
 
     def _force_of_infection(self):
         """Calculates force of infection"""
         self.lambda_v = self.params['alpha_v']*self.params['beta_b']/self.Nb
         self.lambda_b = self.params['alpha_b']*self.params['beta_b']/self.Nb
     
-    def _mosq_population_values(self, t):
-        self.K_v = self.params['K'] + self.params['K_s'] * math.sin((2 * math.pi / 365) * t - math.pi / 2)
-        self.r_v = self.params['r'] + self.params['r_s'] * math.sin((2 * math.pi / 365) * t - math.pi / 2)
+#     def _mosq_population_values(self, t):
+#         self.K_v = self.params['K'] + self.params['K_s'] * math.sin((2 * math.pi / 365) * t - math.pi / 2)
+#         self.r_v = self.params['r'] + self.params['r_s'] * math.sin((2 * math.pi / 365) * t - math.pi / 2)
+
+    def _birth_rate(self):
+        """Caclualtes vector natural birth rate"""
+        self.psi_v = self.params['r_v'] + self.params['mu_v']
 
     def model_func(self, t, y):
         """Defines system of ODEs for dengue model
 
         Initial State Names:
-            Sv: Susceptible mosquito population.\n
-            Ev: Exposed mosquito population. \n
-            Iv: Infected mosquito population.\n
-            Nv: Mosquito population.\n
+            Sv: Susceptible vector population.\n
+            Ev: Exposed vector population. \n
+            Iv: Infected vector population.\n
+            Nv: Vector population.\n
             Sb: Susceptible bird population.\n
             Eb: Exposed bird population.\n
             Ib: Infected bird population.\n
+            Rb: Recovered bird population. \n
             Nb: Bird population.\n
             Ih: Infectious human population.\n
 
         Parameters:
-            mu_v: Mosquito death rate.\n
+            mu_v: Vector death rate.\n
             mu_b: Bird death/recovery rate. \n
             beta_b: Biting rate under frequency dependence. \n
-            alpha_v: Probability of virus transmission to mosquito per infectious bite.\n
-            alpha_b: Probability of virus transmission to bird,per infectious bit.\n
-            K: For K_v(t), baseline mosquito carrying capacity.\n
-            K_s: For K_v(t), scaling factor for the mosquito carrying capacity.\n
-            K_v: Time varying carrying capacity for mosquitoes.\n
-            r: For r_v(t), baseline mosquito growth rate.\n
-            r_s: For r_v(t), scaling factor for mosquito growth rate.\n
-            r_v: Time varying growth rate for mosquitoes.\n
-            lambda_v: Mosquito force of infection. \n
+            alpha_v: Probability of virus transmission to vector, per infectious bite.\n
+            alpha_b: Probability of virus transmission to bird, per infectious bit.\n
+            psi_v: Vector birth rate. \n
+            K_v: Vector carrying capacity.\n
+            r_v: Vector birth rate.\n
+            lambda_v: vector force of infection. \n
             lambda_b: Bird force of infection. \n
-            nu_v: Mosquito latent period. \n
+            nu_v: Vector latent period. \n
             nu_b: Bird latent period. \n
             eta: Contact rate * probability of transmission to humans
 
@@ -101,13 +103,19 @@ class WNVSEIRModel(vbdm.VectorBorneDiseaseModel):
 
         # Find forces of infection
         self._force_of_infection()
+        
+        # Find vector natural birth rate
+        self._birth_rate()
 
-        #Find mosquito carrying capacity and growth rate
-        self._mosq_population_values(t)
+        #Find vector carrying capacity and growth rate
+#        self._mosq_population_values(t)
 
-        ddt['Sv'] = self.r_v * (1 - (self.Nv / self.K_v)) * self.Nv - \
-            self.lambda_v * self.states['Sv'] * self.states['Ib'] - \
-            self.params['mu_v'] * self.states['Sv']
+#         ddt['Sv'] = self.r_v * (1 - (self.Nv / self.K_v)) * self.Nv - \
+#             self.lambda_v * self.states['Sv'] * self.states['Ib'] - \
+#             self.params['mu_v'] * self.states['Sv']
+        ddt['Sv'] = (self.psi_v - self.params['r_v'] * self.Nv / self.params['K_v']) * self.Nv - \
+             self.lambda_v * self.states['Sv'] * self.states['Ib'] - \
+             self.params['mu_v'] * self.states['Sv']
         ddt['Ev'] = self.lambda_v * self.states['Sv'] * self.states['Ib'] - \
             self.params['nu_v'] * self.states['Ev'] - \
             self.params['mu_v'] * self.states['Ev']
@@ -118,6 +126,7 @@ class WNVSEIRModel(vbdm.VectorBorneDiseaseModel):
             self.params['nu_b'] * self.states['Eb']
         ddt['Ib'] = self.params['nu_b'] * self.states['Eb'] - \
             self.params['mu_b'] * self.states['Ib']
+        ddt['Rb'] = self.params['mu_b'] * self.states['Ib']
 
 
         rng = np.random.default_rng()
