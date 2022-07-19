2295e1829f2d0e8150ec5b56b866b40180771302
Jeffrey Keithley <jkeithley@lanl.gov>
2022-07-19 08:54:57 -0600

    Updated to construct constant spline

--- Epi_SEIR/make_dummy_spline.py
+++ Epi_SEIR/make_dummy_spline.py
@@ -10,28 +10,20 @@ import numpy as np
 import pickle
 import yaml
 
-def sine(x):
-    return 0.0002 * math.sin(x/0.0004) + 0.0014
-
 def main():
     with open('config/local_timedeptest_config.yaml', 'r') as in_file:
         config_dict = yaml.safe_load(in_file)
 
     duration = config_dict['DURATION']
     resolution = config_dict['RESOLUTION']
-    print(duration, resolution)
-    num_points = duration*resolution+1
-    x = linspace(0, duration, num_points)
-    y = np.vectorize(sine)(x) # just generates dummy data
+    biting_rate = 0.0014
 
-    number_knots = int(num_points * 0.15) #number of internal knots
-    knots = list(linspace(0, duration, number_knots+2))
-    knots.remove(0)
-    knots.remove(duration)
+    print(duration, resolution)
+
     print('x:', x)
     print('y:', y)
 
-    spl = CubicSpline(x, y)
+    spl = CubicSpline(np.linspace(0, duration, 3), biting_rate*np.ones(3))
     
     # evaluate spline at time step 4.1
     #print(spl([4.1])[0])
